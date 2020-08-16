b7e727df21dc72f508c3f924872f9c717171d164
Gary Saunders <gary.saunders@hunterindustries.com>
2020-08-15 17:20:25 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+      // Material Style Basic Audio Player Title and Audio URL
+      msbapTitle = 'Limo Bus';
+      msbapAudioUrl = 'http://g-slik.com/BEATS/Limo_Bus_INSTRUMENTAL.mp3';   
+      msaapDisplayVolumeControls = true;
+  
+      msbapDisplayTitle = false;   
   title = 'music-player';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../material-module';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    NgxAudioPlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
