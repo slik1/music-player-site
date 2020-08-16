0462dc49636d6f6e189567b15737232eb4dc28ad
Gary Saunders <gary.saunders@hunterindustries.com>
2020-08-15 18:07:04 -0700

    Needs work

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -11,7 +11,11 @@ import { Component, OnInit } from '@angular/core';
 export class PlayerComponent implements OnInit {
   
   
-  
+  // msbapTitle = 'Limo Bus';
+  // msbapAudioUrl = 'http://g-slik.com/BEATS/Limo_Bus_INSTRUMENTAL.mp3';   
+  // msaapDisplayVolumeControls = true;
+
+  // msbapDisplayTitle = false;   
   
 
 
